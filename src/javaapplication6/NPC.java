2b9a3388dfc5440d7b919521d005c1b4c8f4fd2a
350266246 <350266246@C-1B0VHB2.tdsb.on.ca>
2026-01-08 11:38:33 -0500

    3commit

--- src/javaapplication6/NPC.java
+++ src/javaapplication6/NPC.java
@@ -1,69 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package javaapplication6;
-import java.awt.Point;
+import processing.core.PApplet;
+import processing.core.PImage;
+
 /**
  *
- * @author aiden f
+ * @author 350266246
  */
-
-
-public class NPC {
-
-    // 属性
-    private String name;
-    private Point position;
-    private String dialogue;
-    private String hiddenType; // 用于标记是否是魑魅/魍魉
-    private boolean revealed;
-
-    // 构造函数
-    public NPC(String name, Point position, String dialogue, String hiddenType) {
-        this.name = name;
-        this.position = position;
-        this.dialogue = dialogue;
-        this.hiddenType = hiddenType;
-        this.revealed = false;
+public class NPC extends Actor{
+    
+    private String [] text;
+    private boolean talking = false;//检测npc有没有开始说话
+    private int dx;//用来检测和玩家的x距离
+    private int dy;//用来检测和玩家的y距离
+    private int w;
+    private int h;
+    private int lineIndex;
+    public NPC(PApplet app, int x, int y,SpriteSet sprites,String [] text,int w,int h){
+        super(app,1,0,0,x,y,sprites.walk,sprites.stand);
+        this.w = w;
+        this.h = h;
+        this.text = text;
     }
-
-    // ===== 方法 =====
-
-    public void interact(Player player) {
-        // 与玩家互动逻辑待实现
-        // 可以触发对话、任务或者战斗
+    public void starttalking(){
+        talking = true;
+        lineIndex = 0;
     }
-
-    public void reveal() {
-        // 揭示隐藏类型逻辑
-        revealed = true;
+    public boolean istalking(){
+        if(talking) return true;
     }
-
-    // ===== Getter / Setter（可选加分） =====
-
-    public String getName() {
-        return name;
+    public void nextTalk(){
+        if(!talking) return;
+        
+        lineIndex ++;
+        if(lineIndex > text.length){
+            lineIndex = 0;
+            talking = false;
+        }
     }
-
-    public Point getPosition() {
-        return position;
-    }
-
-    public void setPosition(Point position) {
-        this.position = position;
-    }
-
-    public String getDialogue() {
-        return dialogue;
-    }
-
-    public boolean isRevealed() {
-        return revealed;
-    }
-
-    public String getHiddenType() {
-        return hiddenType;
-    }
-}
-
+        
+     }
