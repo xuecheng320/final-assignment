03f1cfa22ce4665629915708918abe58956e5b0c
aiden f <aiden f@DESKTOP-6LNMN1J>
2026-01-08 23:39:42 -0500

    xc

--- src/javaapplication6/NPC.java
+++ src/javaapplication6/NPC.java
@@ -12,8 +12,6 @@ public class NPC extends Actor{
     private boolean talking = false;//检测npc有没有开始说话
     private int dx;//用来检测和玩家的x距离
     private int dy;//用来检测和玩家的y距离
-    private int w;
-    private int h;
     private int lineIndex;
     public NPC(PApplet app, int x, int y,SpriteSet sprites,String [] text,int w,int h){
         super(app,1,0,0,x,y,sprites.walk,sprites.stand);
@@ -26,13 +24,13 @@ public class NPC extends Actor{
         lineIndex = 0;
     }
     public boolean istalking(){
-        if(talking) return true;
+        return talking;
     }
     public void nextTalk(){
         if(!talking) return;
         
         lineIndex ++;
-        if(lineIndex > text.length){
+        if(lineIndex >= text.length){
             lineIndex = 0;
             talking = false;
         }
